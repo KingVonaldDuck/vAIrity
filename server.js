7bc05ccbee417e6ad4cf3e8ff12a32afc1a2bb3b
KingVonaldDuck <logange1223@gmail.com>
2026-03-22 18:38:44 -0400

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,12 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname)));
+app.use(express.static(path.join(__dirname), {
+  setHeaders: (res, filePath) => {
+    if (filePath.endsWith(".css")) res.setHeader("Content-Type", "text/css");
+    if (filePath.endsWith(".js")) res.setHeader("Content-Type", "application/javascript");
+  }
+}));
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
